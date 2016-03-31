9fcae471223109f2ce5f1b6938ee1714ea459229
yzkuan <yzkuan@gmail.com>
2016-03-31 11:05:08 +0000

    Add -y; install vim

--- moonlight.sh
+++ moonlight.sh
@@ -1,6 +1,7 @@
 sudo sh -c 'echo deb http://archive.itimmer.nl/raspbian/moonlight jessie main >> /etc/apt/sources.list'
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
 
-sudo apt-get install moonlight-embedded
+sudo apt-get install -y vim
+sudo apt-get install -y moonlight-embedded
