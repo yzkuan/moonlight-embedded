1f50f5706b90efa2fbef97bb26acdb76cb3c29f4
yzkuan <yzkuan@gmail.com>
2016-03-31 10:29:11 +0000

    install moonlight-embedded

--- moonlight.sh
+++ moonlight.sh
@@ -1,1 +1,6 @@
+sudo sh -c 'echo deb http://archive.itimmer.nl/raspbian/moonlight jessie main >> /etc/apt/sources.list'
+
 sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install moonlight-embedded
